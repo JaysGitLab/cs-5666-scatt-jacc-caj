1049948540e35497a6fdca973265ed582ff9e4f6
Clint Hall <hallbc@appstate.edu>
2017-03-10 09:53:57 -0500

    We have failing tests! Yay!

---
+++ src/main/java/Sb2.java
@@ -0,0 +1,38 @@
+import org.json.JSONObject;
+/**
+ * @version 1.0
+ * @author B. Clint Hall
+ */
+public class Sb2 {
+    /**
+     * Construct an Sb2 object from a filePath.
+     * @param filePath Path to sb2 file.
+     */
+    public Sb2(String filePath) {
+         
+    }
+    /**
+     * Return underlying JSONObject.
+     * @return The underlying JSONObject.
+     */
+    public JSONObject getJSONObject() {
+        return null;
+    }
+
+    /**
+     * Return the number of Sprites in Sb2 object.
+     * @return The number of sprites in project
+     */
+    public int getSpriteCount() {
+        return 0;
+    }
+
+    /**
+     * Given a file path return a String of file contents.
+     * @param path Path to the file
+     * @return contents of file
+     */
+    public static String getFileContents(String path) {
+        return null;
+    }
+}
--- src/test/java/ScattTest.java
+++ src/test/java/ScattTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -17,5 +18,32 @@ public class ScattTest {
     public void testHello() {
         Scatt s = new Scatt();
         assertEquals(s.hello(), "Hello from Scatt");
+    }
+    /**
+     * Test Sb2.getSpriteCount.
+     */
+    @Test
+    public void testGetSpriteCount() {
+        String filePath = "WizardSpells.sb2";
+        Sb2 wizardSpells = new Sb2(filePath);
+        int spriteCount = wizardSpells.getSpriteCount();
+        assertTrue(spriteCount == 3);
+    }
+    /**
+     * Test sb2 constructor with valid file path.
+     */
+    @Test
+    public void testSb2Ctor1() {
+        String filePath = "WizardSpells.sb2";
+        Sb2 wizardSpells = new Sb2(filePath);
+        assertTrue(wizardSpells.getJSONObject() instanceof org.json.JSONObject);
+    }
+    /**
+     * Test getFileContents with valid file path.
+     */
+    @Test
+    public void testGetFileContents1() {
+        String contents = Sb2.getFileContents("DummyForTestGetFileContents1.txt");
+        assertEquals("You got the contents of DummyForTestGetFileContents1.txt", contents);
     }
 }
