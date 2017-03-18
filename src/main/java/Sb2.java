08f45e3c22985ebfde1942817baf182feaca6836
Clint Hall <hallbc@appstate.edu>
2017-03-18 19:23:23 -0400

    progress

--- src/main/java/Sb2.java
+++ src/main/java/Sb2.java
@@ -1,22 +1,30 @@
 import org.json.JSONObject;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+import java.io.IOException;
 /**
  * @version 1.0
  * @author B. Clint Hall
  */
 public class Sb2 {
+    private JSONObject sb2Json;
     /**
      * Construct an Sb2 object from a filePath.
      * @param filePath Path to sb2 file.
      */
     public Sb2(String filePath) {
-         
+        // unzip the file.
+        // get the path to the unzipped json file
+        sb2Json = new JSONObject(/*json*/filePath);
     }
     /**
      * Return underlying JSONObject.
      * @return The underlying JSONObject.
      */
     public JSONObject getJSONObject() {
-        return null;
+        return sb2Json;
     }
 
     /**
@@ -26,13 +34,38 @@ public class Sb2 {
     public int getSpriteCount() {
         return 0;
     }
-
+    /**
+     * Given a path to a json file, return a JSONObject.
+     * @param pathStr Path to the json file
+     */
+    public static JSONObject getJSONObject(String pathStr){
+        String contents = getFileContents(pathStr);
+        return new JSONObject(contents);
+    }
     /**
      * Given a file path return a String of file contents.
-     * @param path Path to the file
+     * @param pathStr Path to the file
      * @return contents of file
      */
-    public static String getFileContents(String path) {
-        return null;
+    public static String getFileContents(String pathStr) {
+        Path path = Paths.get(pathStr);
+        String fileString = null;
+        try {
+            fileString = Files.lines(path).collect(Collectors.joining("\n"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileString;
     }
+    /**
+     * Print the current working directory.  Useful for debugging.
+     * Thank you http://stackoverflow.com/a/15954821
+     */
+    public static void printCwd() {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current relative path is: " + s);
+    } 
+
 }
--- src/test/java/ScattTest.java
+++ src/test/java/ScattTest.java
@@ -1,6 +1,9 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import java.net.URL;
+import java.io.File;
+import org.json.JSONObject;
 
 /**
  * @version 1.0
@@ -22,28 +25,47 @@ public class ScattTest {
     /**
      * Test Sb2.getSpriteCount.
      */
-    @Test
+   /* @Test
     public void testGetSpriteCount() {
-        String filePath = "WizardSpells.sb2";
+        String filePath = this
+            .getClass()
+            .getResource("WizardSpells.sb2")
+            .getFile();
         Sb2 wizardSpells = new Sb2(filePath);
         int spriteCount = wizardSpells.getSpriteCount();
         assertTrue(spriteCount == 3);
-    }
+    }*/
     /**
      * Test sb2 constructor with valid file path.
      */
     @Test
     public void testSb2Ctor1() {
-        String filePath = "WizardSpells.sb2";
+        URL url = this.getClass().getResource("/WizardSpells.sb2");
+        String filePath = url.getFile();
         Sb2 wizardSpells = new Sb2(filePath);
         assertTrue(wizardSpells.getJSONObject() instanceof org.json.JSONObject);
     }
+    /**
+     * Test getJSONObject.
+     */
+    @Test
+    public void testGetJSONObject(){
+        URL url = this.getClass().getResource("/project.json");
+        String filePath = url.getFile();
+        JSONObject jsonObj = Sb2.getJSONObject(filePath);
+        assertTrue(jsonObj instanceof org.json.JSONObject);
+    }
+        
     /**
      * Test getFileContents with valid file path.
      */
     @Test
     public void testGetFileContents1() {
-        String contents = Sb2.getFileContents("DummyForTestGetFileContents1.txt");
+        String filePath = this
+            .getClass()
+            .getResource("/DummyForTestGetFileContents1.txt")
+            .getFile();
+        String contents = Sb2.getFileContents(filePath);
         assertEquals("You got the contents of DummyForTestGetFileContents1.txt", contents);
     }
 }
