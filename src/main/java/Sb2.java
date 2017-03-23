bf73c0f546169a784adeb40eab0a9356ccae1527
B. Clint Hall <bclinthall@gmail.com>
2017-03-22 22:06:13 -0400

    Refactor and get old tests to pass

--- src/main/java/Sb2.java
+++ src/main/java/Sb2.java
@@ -83,23 +83,54 @@ public class Sb2 {
         String[] spriteNames = new String[spriteMap.size()];
         return spriteMap.keySet().toArray(spriteNames);
     }
+
+    
+    /**
+     * Each script is represented by an json array of block tuples,
+     * where each block tuple is like a line of code. A Sprite may have
+     * multiple Scripts associated with it.  This function returns an array
+     * of json arrays of block tuples for the requested Sprite.
+     * @param spriteName The name of the sprite
+     * @return An array block tuples.  Each block tuple is represented as
+     *         a JSONArray.
+     */
+     private JSONArray[] getScripts(String spriteName){
+        JSONObject sprite = spriteMap.get(spriteName);
+        if (sprite == null) {
+            throw new IllegalArgumentException(
+                spriteName + " is not the name of a Sprite in this project");
+        }
+        JSONArray scriptTuples = sprite.optJSONArray("scripts");
+        if (scriptTuples == null) {
+            return new JSONArray[0];
+        } else {
+            JSONArray[] blockTuples = new JSONArray[scriptTuples.length()];
+            for(int i = 0; i < blockTuples.length; i++){
+                blockTuples[i] = getBlockTuple(scriptTuples.optJSONArray(i));
+            }
+            return blockTuples;
+        }
+    }
+    /**
+     * Given a script tuple, return it's block tuple
+     * @param scriptTuple A script tuple JSONArray
+     * @return a blockTuple JSONArray
+     */
+    private JSONArray getBlockTuple(JSONArray scriptTuple) {
+        if (scriptTuple == null) {
+            return new JSONArray();
+        } else {
+            JSONArray blockTuple = scriptTuple.getJSONArray(2);
+            return blockTuple == null ? new JSONArray() : blockTuple;
+        }
+    }
     /**
      * Return the number of Scripts associated with a particular Sprite.
      * @param spriteName The name of the Sprite.
      * @return The number of Scripts associated with the Sprite.
      */
     public int getScriptCountForSprite(String spriteName) {
-        JSONObject sprite = spriteMap.get(spriteName);
-        if (sprite == null) {
-            throw new IllegalArgumentException(
-                spriteName + " is not the name of a Sprite in this project");
-        }
-        JSONArray scripts = sprite.optJSONArray("scripts");
-        if (scripts == null) {
-            return 0;
-        } else {
-            return scripts.length();
-        }
+        return getScripts(spriteName).length;
     }
      
     /**
