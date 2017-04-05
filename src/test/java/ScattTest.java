c1d2617c442a95d9bcfcb9e5568fa309638f76c9
B. Clint Hall <bclinthall@gmail.com>
2017-04-04 22:06:52 -0400

    remove an obsolete test

--- src/test/java/ScattTest.java
+++ src/test/java/ScattTest.java
@@ -75,17 +75,4 @@ public class ScattTest {
         JSONObject jsonObj = Utils.getWizardJSONObject();
         assertTrue(jsonObj instanceof org.json.JSONObject);
     }
-    /**
-     * Early Method for testing GUI
-     */
-    /*@Test
-    public void testGui() {
-       // Scatt s = new Scatt();
-       // s.showUI();
-       // String testFile="src/test/java/TestUtils.java";
-       // File f = new File(testFile); 
-       // FileChooser fc = new FileChooser();
-       // fc.setFile(f);
-       // assertEquals(fc.getPath(),testFile);
-    }*/
 }
