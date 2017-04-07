af10fce2a6c234da7cf8ad7e9a82f0080b554726
James Ward <wardja2@appstate.edu>
2017-04-07 01:00:33 -0400

    Began writing variables test. Need to further analyze where to begin.

---
+++ src/test/java/VariablesTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.StringWriter;
+
+/**
+ * This test class is meant to test that our Sb2 program can correctly count
+ * the number of global variables, script variables, and sprite variables.
+ * @version 1
+ * @author James Ward
+ */
+
+public class VariableTest {
+    /*
+     * Test that the Sb2 can accurately count the number of global variables.
+     */
+    @Test
+    public void testGlobalVariablesCount(){
+        List<Sb2> sb2s = new ArrayList<Sb2>();
+        Sb2 scattariaSb2 = new Sb2(Utils.get
+    }
+}
