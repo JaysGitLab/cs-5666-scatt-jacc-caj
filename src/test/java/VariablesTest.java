b35721778edfe3bb96bb376edeaf953c1773db35
James Ward <wardja2@appstate.edu>
2017-04-14 02:55:41 -0400

    variable tests

--- src/test/java/VariablesTest.java
+++ src/test/java/VariablesTest.java
@@ -16,9 +16,9 @@ public class VariablesTest {
     @Test
     public void testGlobalVariablesCount() {
         Sb2 scattariaSb2 = new Sb2(Utils.getScratchariaJSONObject());
-        int expectedGlobVar = scattariaSb2.getGlobalVariableCount();
-        int actual = 23;
-        assertEquals(expectedGlobVar, actual); 
+        int actual = scattariaSb2.getGlobalVariableCount();
+        int expected = 23;
+        assertEquals(expected, actual); 
     }
     /**
      * Test that the Sb2 can accurately count variables in sprites.
@@ -27,13 +27,13 @@ public class VariablesTest {
     @Test
     public void testSpriteVariableCount() throws IOException {
         Sprites scratchariaSprites = new Sprites(Utils.getScratchariaJSONObject());
-        int expectedSpriteVar;
+        int actual;
         try {
-            expectedSpriteVar = scratchariaSprites.getSpriteVariableCount("Sun2");
+            actual = scratchariaSprites.getSpriteVariableCount("Sun2");
         } catch (IOException e) {
             throw new IOException("Wrong sprite name");
         }
-        int actual = 2;
-        assertEquals(actual, expectedSpriteVar);
+        int expected = 2;
+        assertEquals(expected, actual);
     }
 }
